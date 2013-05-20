8b913c435c0dbdbfa47c2e1c697e2b85e20c9836
ChasWoodfield <chas.woodfield@k-int.co.uk>
2013-05-20 11:26:38 +0100

    Added javadoc generation to the pom
    Added GenerateJavaDoc.bat to generate the javadoc
    Added documentation to a few more file

--- ECKClient/src/com/k_int/euinside/client/module/setmanager/update/Update.java
+++ ECKClient/src/com/k_int/euinside/client/module/setmanager/update/Update.java
@@ -8,8 +8,19 @@ import com.k_int.euinside.client.HttpResult;
 import com.k_int.euinside.client.http.ClientHTTP;
 import com.k_int.euinside.client.module.Attribute;
 
+/**
+ * The Update class handles the specific requirements for performing the SetManagers Update action
+ */
 public class Update {
 
+	/**
+	 * Builds the attribute array for the Update action
+	 * 
+	 * @param deleteAll ........ true if all records are to be deleted from the set 
+	 * @param recordsToDelete .. An array of record ids that should be deleted
+	 *  
+	 * @return An array of NameValuePairs that will be used for the query part of the url
+	 */
 	static private ArrayList<BasicNameValuePair> buildattributeArray(boolean deleteAll, ArrayList<String> recordsToDelete) {
 		ArrayList<BasicNameValuePair> attributes = new ArrayList<BasicNameValuePair>();
 		
@@ -32,10 +43,31 @@ public class Update {
 		return(attributes);
 	}
 
+	/**
+	 * Updates the SetManager with the supplied details
+	 * 
+	 * @param path ............ The URL path for the action 
+	 * @param xmlData ......... An array of xml records that are to be posted
+	 * @param zipData ......... An array of zip files that are to be posted
+	 * @param deleteAll ....... true if all records in the set are to be deleted
+	 * @param recordsToDelete . An array of records that are to be deleted from the set
+	 *  
+	 * @return A HttpResult object, if the httpStatusCode is HttpServletResponse.SC_ACCEPTED, then the call has been successful 
+	 */
 	static public HttpResult sendBytes(String path, ArrayList<byte[]> xmlData, ArrayList<byte[]> zipData, boolean deleteAll, ArrayList<String> recordsToDelete) {
 		return(ClientHTTP.sendBytes(path, xmlData, zipData, buildattributeArray(deleteAll, recordsToDelete)));
 	}
 
+	/**
+	 * Updates the SetManager with the supplied details
+	 * 
+	 * @param path ............ The URL path for the action 
+	 * @param filenames ....... An array of files that are to be posted (only files with extensions .xml and .zip are posted)
+	 * @param deleteAll ....... true if all records in the set are to be deleted
+	 * @param recordsToDelete . An array of records that are to be deleted from the set
+	 *  
+	 * @return A HttpResult object, if the httpStatusCode is HttpServletResponse.SC_ACCEPTED, then the call has been successful 
+	 */
 	static public HttpResult sendFiles(String path, ArrayList<String> filenames, boolean deleteAll, ArrayList<String> recordsToDelete) {
 		return(ClientHTTP.sendFiles(path, filenames, buildattributeArray(deleteAll, recordsToDelete)));
 	}
--- ECKClient/src/com/k_int/euinside/client/module/validation/semantika/Error.java
+++ ECKClient/src/com/k_int/euinside/client/module/validation/semantika/Error.java
@@ -2,6 +2,9 @@ package com.k_int.euinside.client.module.validation.semantika;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The Error class contains a validation error returned from the semantika validation service
+ */
 public class Error {
 	@JsonProperty("BrokenRuleCode")
 	String brokenRuleCode;
@@ -12,31 +15,64 @@ public class Error {
 
 	public Error() {
 	}
-	
+
+	/**
+	 * Retrieves the error code for this error
+	 * 
+	 * @return The error code
+	 */
 	public String getBrokenRuleCode() {
 		return(brokenRuleCode);
 	}
-	
+
+	/**
+	 * Sets the error code
+	 * 
+	 * @param brokenRuleCode The error code
+	 */
 	public void setBrokenRuleCode(String brokenRuleCode) {
 		this.brokenRuleCode = brokenRuleCode;
 	}
-	
+
+	/**
+	 * Retrieves an informative message that was returned with this error code
+	 * 
+	 * @return The informative message
+	 */
 	public String getMessage() {
 		return(message);
 	}
-	
+
+	/**
+	 * Sets the informative message for this validation error
+	 * 
+	 * @param message The informative message
+	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
+
+	/**
+	 * Retrieve the severity of this error
+	 * 
+	 * @return The severity of this error
+	 */
 	public int getSeverity() {
 		return(severity);
 	}
-	
+
+	/**
+	 * Sets the severity of this error
+	 * 
+	 * @param severity The severity of this error
+	 */
 	public void setSeverity(int severity) {
 		this.severity = severity;
 	}
 	
+	/**
+	 * Formats the members of this class in a simple to view way
+	 */
 	public String toString() {
 		String result = "Class: Error:\n"; 
 		result += "\tbrokenRuleCode: " + brokenRuleCode + "\n";
